14d4b1523ce6732a6d7d6d16db9de81c2daad82c
Sohel Mia <sohelcse1999@gmail.com>
2022-07-15 20:09:42 +0600

    remove .next

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <>
-      <div>logo</div>
       <Image src="/logo.png" alt="Kodezam" width="136" height="20.3" />
     </>
   );
